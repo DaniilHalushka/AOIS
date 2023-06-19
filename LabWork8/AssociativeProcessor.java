52ceb002ef26ba5241152488678fe376c778feff
Daniil Halushka <92893370+DaniilHalushka@users.noreply.github.com>
2023-06-19 14:14:49 +0300

    Remake of 8 lab

--- LabWork8/AssociativeProcessor.java
+++ LabWork8/AssociativeProcessor.java
@@ -1,349 +1,174 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.Random;
 
 public class AssociativeProcessor {
-    private static final int BIT_SIZE = 16;
-    private int[][] matrix;
-    private boolean diagonal;
+    private final int sizeOfTable;
+    int[][] memoryTable;
 
-    public AssociativeProcessor(int[][] data) {
-        this.matrix = data;
-        this.diagonal = false;
+    public AssociativeProcessor(int size) {
+        this.sizeOfTable = size;
+        this.memoryTable = new int[16][16];
+        for (int i = 0; i < this.sizeOfTable; i++) {
+            this.memoryTable[i] = Arrays.copyOf(this.memoryTable[i], 16);
+        }
     }
 
-    public void searchingByAccordance() {
-        Map<String, Integer> tickOfMatching = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input your example data: ");
-        String exampleOfData = scanner.nextLine();
-        for (char charValue : exampleOfData.toCharArray()) {
-            if (charValue != '0' && charValue != '1') {
-                System.out.println("Wrong example data!");
-                return;
+    private int[] wordMove(int index, int[] word) {
+        int movingPart = word.length - index;
+        int[] firstPart = Arrays.copyOfRange(word, 0, movingPart);
+        int[] secondPart = Arrays.copyOfRange(word, movingPart, word.length);
+
+        int[] movedWord = new int[word.length];
+        System.arraycopy(secondPart, 0, movedWord, 0, secondPart.length);
+        System.arraycopy(firstPart, 0, movedWord, secondPart.length, firstPart.length);
+
+        return movedWord;
+    }
+
+    public void makeEntry(int[] word, int index) {
+        int[] movedWord = wordMove(index, word);
+        for (int i = 0; i < memoryTable.length; i++) {
+            memoryTable[i][index] = movedWord[i];
+        }
+    }
+
+    public int[][] generateWords(int size) {
+        int[][] matrixOfWords = new int[size][size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrixOfWords[i][j] = random.nextInt(2);
             }
         }
-        if (exampleOfData.length() > BIT_SIZE) {
-            exampleOfData = exampleOfData.substring(0, BIT_SIZE);
-        } else if (exampleOfData.length() < BIT_SIZE) {
-            exampleOfData = String.format("%" + BIT_SIZE + "s", exampleOfData).replace(' ', '0');
-        }
-        System.out.println("Our data for matching: " + exampleOfData);
-        int[][] copyOfMatrix = Arrays.copyOf(matrix, matrix.length);
-        boolean diagonalDataValues = diagonal;
-        if (diagonalDataValues) {
-            convertToStraight();
-        }
-        for (int numberValue = 0; numberValue < matrix.length; numberValue++) {
-            tickOfMatching.put(Arrays.toString(matrix[numberValue]), recurseAlgorithmForAccordance(copyOfMatrix[numberValue], exampleOfData, numberValue));
-        }
-        if (diagonalDataValues) {
-            convertToDiagonal();
-        }
-        System.out.println("Strings with the highest matching digits:");
-        tickOfMatching.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .skip(Math.max(0, tickOfMatching.size() - 3))
-                .forEach(entry -> System.out.println(entry.getKey()));
+        return matrixOfWords;
     }
 
-
-    public void convertToStraight() {
-        int[][] copyOfMatrix = Arrays.copyOf(matrix, matrix.length);
-        matrix = new int[copyOfMatrix.length][];
-        for (int numberValue = 0; numberValue < copyOfMatrix.length; numberValue++) {
-            matrix[numberValue] = new int[copyOfMatrix[numberValue].length];
-            System.arraycopy(copyOfMatrix[numberValue], numberValue, matrix[numberValue], 0, copyOfMatrix[numberValue].length - numberValue);
-            System.arraycopy(copyOfMatrix[numberValue], 0, matrix[numberValue], copyOfMatrix[numberValue].length - numberValue, numberValue);
+    int[] takeWord(int index) {
+        int[] movedWord = new int[memoryTable.length];
+        for (int i = 0; i < memoryTable.length; i++) {
+            movedWord[i] = memoryTable[i][index];
         }
-        diagonal = false;
+
+        int[] chosenWord = new int[memoryTable.length];
+        System.arraycopy(movedWord, index, chosenWord, 0, movedWord.length - index);
+        System.arraycopy(movedWord, 0, chosenWord, movedWord.length - index, index);
+
+        return chosenWord;
     }
 
-    public void convertToDiagonal() {
-        int[][] copyOfMatrix = Arrays.copyOf(matrix, matrix.length);
-        matrix = new int[copyOfMatrix.length][];
-        for (int numberValue = 0; numberValue < copyOfMatrix.length; numberValue++) {
-            matrix[numberValue] = new int[copyOfMatrix[numberValue].length];
-            System.arraycopy(copyOfMatrix[numberValue], copyOfMatrix[numberValue].length - numberValue - 1, matrix[numberValue], 0, numberValue + 1);
-            System.arraycopy(copyOfMatrix[numberValue], 0, matrix[numberValue], numberValue + 1, copyOfMatrix[numberValue].length - numberValue - 1);
+    public int[][] obtainWord() {
+        int[][] word = new int[sizeOfTable][sizeOfTable];
+        for (int i = 0; i < sizeOfTable; i++) {
+            word[i] = takeWord(i);
         }
-        diagonal = true;
+
+        return word;
     }
 
+    public Map<Integer, int[]> arithmeticsLogic() {
+        List<int[]> results = new ArrayList<>();
+        int[][] listOfAllWords = obtainWord();
+        for (int[] word : listOfAllWords) {
+            int[] A = Arrays.copyOfRange(word, 3, 7);
+            int[] B = Arrays.copyOfRange(word, 7, 11);
+            int[] sum = unionOfTheParts(A, B);
+            int[] result = Arrays.copyOf(word, 11 + sum.length);
+            System.arraycopy(sum, 0, result, 11, sum.length);
+            results.add(result);
+        }
 
-    public void logicOperations() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose type of operation:");
-        System.out.println("1 -- constant 1");
-        System.out.println("2 -- constant 0");
-        System.out.println("3 -- New argument");
-        System.out.println("4 -- Negative of new argument");
-        int typeOfOperation = scanner.nextInt();
-        System.out.print("Choose column: ");
-        int numberOfColumn = scanner.nextInt();
+        Map<Integer, int[]> finalResults = new HashMap<>();
+        for (int i = 0; i < results.size(); i++) {
+            finalResults.put(i, results.get(i));
+        }
 
-        switch (typeOfOperation) {
-            case 1 -> {
-                for (int i = 0; i < matrix.length; i++) {
-                    matrix[i][numberOfColumn] = 1;
-                }
+        return finalResults;
+    }
+
+    private int[] unionOfTheParts(int[] firstWord, int[] secondWord) {
+        int[] firstElement = Arrays.copyOf(firstWord, firstWord.length);
+        int[] secondElement = Arrays.copyOf(secondWord, secondWord.length);
+        int[] result = new int[Math.max(firstElement.length, secondElement.length)];
+        int utilElement = 0;
+
+        while (firstElement.length > 0 && secondElement.length > 0) {
+            int firstDigit = firstElement[firstElement.length - 1];
+            int secondDigit = secondElement[secondElement.length - 1];
+            int utilResult = firstDigit ^ secondDigit ^ utilElement;
+            utilElement = (firstDigit & secondDigit) | ((firstDigit ^ secondDigit) & utilElement);
+            result[result.length - 1] = utilResult;
+            firstElement = Arrays.copyOf(firstElement, firstElement.length - 1);
+            secondElement = Arrays.copyOf(secondElement, secondElement.length - 1);
+            result = Arrays.copyOf(result, result.length - 1);
+        }
+
+        result = Arrays.copyOf(result, result.length + 1);
+        result[result.length - 1] = utilElement;
+
+        return result;
+    }
+
+    public int[][] searchNearestPattern(int[] pattern) {
+        List<Integer> differenceList = new ArrayList<>();
+        for (int[] word : memoryTable) {
+            int utilDifference = 0;
+            for (int i = 0; i < sizeOfTable; i++) {
+                utilDifference += word[i] != pattern[i] ? 1 : 0;
             }
-            case 2 -> {
-                for (int i = 0; i < matrix.length; i++) {
-                    matrix[i][numberOfColumn] = 0;
-                }
-            }
-            case 3 -> positiveArgument(numberOfColumn);
-            case 4 -> negativeArgument(numberOfColumn);
+            differenceList.add(utilDifference);
         }
-
-        int columns = matrix[0].length;
-        for (int[] ints : matrix) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(ints[j] + " ");
-            }
-            System.out.println();
+        int minimalDifference = Integer.MAX_VALUE;
+        for (int difference : differenceList) {
+            minimalDifference = Math.min(minimalDifference, difference);
         }
-        System.out.println();
-    }
-
-
-    private void positiveArgument(int numberOfColumn) {
-        boolean diagonalData = diagonal;
-        if (diagonalData) {
-            convertToStraight();
-        }
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input argument: ");
-        String argument = scanner.nextLine();
-        if (argument.length() > BIT_SIZE) {
-            argument = argument.substring(0, BIT_SIZE);
-        } else if (argument.length() < BIT_SIZE) {
-            argument = String.format("%" + BIT_SIZE + "s", argument).replace(' ', '0');
-        }
-        char[] argumentChars = argument.toCharArray();
-        for (int numberValue = 0; numberValue < matrix[numberOfColumn].length; numberValue++) {
-            matrix[numberOfColumn][numberValue] = Character.getNumericValue(argumentChars[numberValue]);
-        }
-        if (diagonalData) {
-            convertToDiagonal();
-        }
-    }
-
-    private void negativeArgument(int numberOfColumn) {
-        boolean diagonalData = diagonal;
-        if (diagonalData) {
-            convertToStraight();
-        }
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input argument: ");
-        String argument = scanner.nextLine();
-        if (argument.length() > BIT_SIZE) {
-            argument = argument.substring(0, BIT_SIZE);
-        } else if (argument.length() < BIT_SIZE) {
-            argument = String.format("%" + BIT_SIZE + "s", argument).replace(' ', '0');
-        }
-        argument = argument.replace("1", "2").replace("0", "1").replace("2", "0");
-        char[] argumentChars = argument.toCharArray();
-        for (int numberValue = 0; numberValue < matrix[numberOfColumn].length; numberValue++) {
-            matrix[numberOfColumn][numberValue] = Character.getNumericValue(argumentChars[numberValue]);
-        }
-        if (diagonalData) {
-            convertToDiagonal();
-        }
-    }
-
-    private static int[] sumOfNumbers(int[] num1, int[] num2) {
-        int[] sum = new int[num1.length];
-        int utilValue = 0;
-        for (int i = num1.length - 1; i >= 0; i--) {
-            if (num1[i] + num2[i] == 1 && utilValue == 0) {
-                sum[i] = 1;
-            } else if (num1[i] + num2[i] == 1 && utilValue > 0) {
-                sum[i] = 0;
-            } else if (num1[i] + num2[i] == 2 && utilValue > 0) {
-                sum[i] = 1;
-            } else if (num1[i] + num2[i] == 0 && utilValue > 0) {
-                sum[i] = 1;
-                utilValue -= 1;
-            } else if (num1[i] + num2[i] == 0 && utilValue == 0) {
-                sum[i] = 0;
-            } else if (num1[i] + num2[i] == 2 && utilValue == 0) {
-                sum[i] = 0;
-                utilValue += 1;
+        List<int[]> matchingItems = new ArrayList<>();
+        for (int i = 0; i < differenceList.size(); i++) {
+            if (differenceList.get(i) == minimalDifference) {
+                matchingItems.add(memoryTable[i]);
             }
         }
-        if (utilValue > 0) {
-            sum = Arrays.copyOf(sum, sum.length + 1);
-            System.arraycopy(sum, 0, sum, 1, sum.length - 1);
-            sum[0] = 1;
-        } else if (utilValue == 0) {
-            sum = Arrays.copyOf(sum, sum.length + 1);
-            System.arraycopy(sum, 0, sum, 1, sum.length - 1);
-            sum[0] = 0;
+        int[][] result = new int[matchingItems.size()][sizeOfTable];
+        for (int i = 0; i < matchingItems.size(); i++) {
+            result[i] = matchingItems.get(i);
         }
-        return sum;
+        return result;
     }
 
-    public void sumOfFields() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input your example data: ");
-        String exampleData = scanner.nextLine().substring(0, 3);
-        boolean diagonalData = diagonal;
-        if (diagonalData) {
-            convertToStraight();
-        }
-        for (int[] ints : matrix) {
-            if (Arrays.toString(ints).replace(", ", "").substring(1, 4).equals(exampleData)) {
-                int[] sum = sumOfNumbers(Arrays.copyOfRange(ints, 3, 7), Arrays.copyOfRange(ints, 7, 11));
-                System.arraycopy(sum, 0, ints, 11, sum.length);
-            }
-        }
-        if (diagonalData) {
-            convertToDiagonal();
-        }
-        int columns = matrix[0].length;
-
-        for (int[] ints : matrix) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(ints[j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private int recurseAlgorithmForAccordance(int[] matrixString, String data, int numberOfString) {
-        int tick = 0;
-        int positionOfBreak = 0;
-        if (!diagonal) {
-            for (int digit = 0; digit < data.length(); digit++) {
-                if (data.charAt(digit) == Character.forDigit(matrixString[digit], 10)) {
-                    tick++;
-                }
-            }
-        }
-        if (diagonal) {
-            for (int digit = 0; digit < data.substring(numberOfString).length(); digit++) {
-                positionOfBreak = digit;
-                if (data.charAt(digit + numberOfString) == Character.forDigit(matrixString[digit], 10)) {
-                    tick++;
-                }
-            }
-            for (int digit = 0; digit < data.substring(0, numberOfString).length(); digit++) {
-                if (data.charAt(digit) == Character.forDigit(matrixString[positionOfBreak + digit], 10)) {
-                    tick++;
-                }
-            }
-        }
-        return tick;
-    }
-
-    public void chooseReadOrWrite() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("1 -- Read column");
-        System.out.println("2 -- Read word");
-        System.out.println("3 -- Write column");
-        System.out.println("4 -- Write word");
-        int typeOfOperation = scanner.nextInt();
-        switch (typeOfOperation) {
-            case 1 -> readWord();
-            case 2 -> readColumn();
-            case 3 -> writeWord();
-            case 4 -> writeColumn();
+    public void showTable() {
+        for (int[] ints : memoryTable) {
+            System.out.println(Arrays.toString(ints));
         }
     }
 
-    private void readWord() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input word position: ");
-        int wordPosition = scanner.nextInt();
-        int[] word = new int[matrix.length];
-        for (int numberValue = 0; numberValue < matrix.length; numberValue++) {
-            word[numberValue] = matrix[numberValue][wordPosition];
+    public void showDiagonalTable() {
+        int[][] diagonalTable = obtainWord();
+        for (int[] ints : diagonalTable) {
+            System.out.println(Arrays.toString(ints));
         }
-        System.out.println(Arrays.toString(word));
     }
 
-    private void readColumn() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input column number: ");
-        int columnNumber = scanner.nextInt();
-        int[] column = matrix[columnNumber];
-        System.out.println(Arrays.toString(column));
+    public int[] F0() {
+        return new int[]{0, 0, 0, 0};
     }
 
-    private void writeWord() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input word position: ");
-        int wordPosition = scanner.nextInt();
-        int[] word = new int[matrix.length];
-        System.out.print("Input word: ");
-        String wordString = scanner.next();
-        if (wordString.length() != matrix.length) {
-            System.out.println("Invalid word length!");
-            return;
-        }
-        for (int numberValue = 0; numberValue < matrix.length; numberValue++) {
-            word[numberValue] = Character.getNumericValue(wordString.charAt(numberValue));
-        }
-        for (int numberValue = 0; numberValue < matrix.length; numberValue++) {
-            matrix[numberValue][wordPosition] = word[numberValue];
-        }
-        int columns = matrix[0].length;
-
-        for (int[] ints : matrix) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(ints[j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    public void F5(int[] argument) {
+        System.out.println(Arrays.toString(argument));
     }
 
-
-    private void writeColumn() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input column number: ");
-        int columnNumber = scanner.nextInt();
-        int[] column = new int[matrix.length];
-        System.out.print("Input column data: ");
-        String columnData = scanner.next();
-        if (columnData.length() != matrix.length) {
-            System.out.println("Invalid column data length!");
-            return;
+    public int[] F10(int[] argument) {
+        int[] result = new int[argument.length];
+        for (int i = 0; i < argument.length; i++) {
+            result[i] = argument[i] == 0 ? 1 : 0;
         }
-        for (int numberValue = 0; numberValue < column.length; numberValue++) {
-            column[numberValue] = Character.getNumericValue(columnData.charAt(numberValue));
-        }
-        matrix[columnNumber] = column;
-        int columns = matrix[0].length;
-
-        for (int[] ints : matrix) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(ints[j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+        return result;
     }
 
-
-    public static void showMatrix(int[][] matrix) {
-        int columns = matrix[0].length;
-
-        for (int[] ints : matrix) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(ints[j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    public int[][] getMatrix() {
-        return matrix;
+    public int[] F15() {
+        return new int[]{1, 1, 1, 1};
     }
 }
--- LabWork8/Main.java
+++ LabWork8/Main.java
@@ -1,57 +1,46 @@
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int rows = 16;
-        int columns = 16;
-        int[][] matrix = new int[rows][columns];
-        int[] addressOfSearching = new int[16];
+        AssociativeProcessor associativeProcessor = new AssociativeProcessor(16);
+        int[][] words = associativeProcessor.generateWords(16);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                matrix[i][j] = (int) (Math.random() * 2);
-            }
-        }
-
+        System.out.println("Words");
         for (int i = 0; i < 16; i++) {
-            addressOfSearching[i] = (int) (Math.random() * 2);
+            associativeProcessor.makeEntry(words[i], i);
         }
+        associativeProcessor.showTable();
 
-        AssociativeProcessor processor = new AssociativeProcessor(matrix);
-        AssociativeProcessor.showMatrix(matrix);
-        choosingTask(processor);
-    }
+        System.out.println("\n***********************************************");
+        System.out.println("\nChosen element (the first word): " + Arrays.toString(associativeProcessor.takeWord(0)));
+        System.out.println("\n***********************************************");
+        System.out.println("\nTable with diagonalizing:");
+        associativeProcessor.showDiagonalTable();
+        System.out.println("\n***********************************************\n");
+        System.out.println("\nStandard table:");
+        associativeProcessor.showTable();
 
-    public static void choosingTask(AssociativeProcessor processor) {
-        while (true) {
-            System.out.println("Choose task: ");
-            System.out.println("1 -- searching by accordance");
-            System.out.println("2 -- convert to diagonal");
-            System.out.println("3 -- convert to straight");
-            System.out.println("4 -- logic operations");
-            System.out.println("5 -- sum of digits");
-            System.out.println("6 -- Read or write column");
+        System.out.println("\n***********************************************\n");
+        System.out.println("Logic functions");
+        System.out.println("F0: " + Arrays.toString(associativeProcessor.F0()));
+        System.out.print("F5: ");
+        associativeProcessor.F5(associativeProcessor.memoryTable[1]);
+        System.out.println("F10: " + Arrays.toString(associativeProcessor.F10(associativeProcessor.memoryTable[2])));
+        System.out.println("F15: " + Arrays.toString(associativeProcessor.F15()));
 
-            Scanner scanner = new Scanner(System.in);
-            int task = scanner.nextInt();
-
-            switch (task) {
-                case 1 -> processor.searchingByAccordance();
-                case 2 -> {
-                    processor.convertToDiagonal();
-                    AssociativeProcessor.showMatrix(processor.getMatrix());
-                }
-                case 3 -> {
-                    processor.convertToStraight();
-                    AssociativeProcessor.showMatrix(processor.getMatrix());
-                }
-                case 4 -> processor.logicOperations();
-                case 5 -> processor.sumOfFields();
-                case 6 -> processor.chooseReadOrWrite();
-                default -> {
-                    return;
-                }
-            }
+        int[] searchWord = new int[]{0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0};
+        System.out.println("\n***********************************************\n");
+        System.out.println("Match search for " + Arrays.toString(searchWord) + ":");
+        int[][] closestPatterns = associativeProcessor.searchNearestPattern(searchWord);
+        for (int[] pattern : closestPatterns) {
+            System.out.println(Arrays.toString(pattern));
+        }
+        System.out.println("\n***********************************************\n");
+        System.out.println("Sum of elements");
+        Map<Integer, int[]> arithmeticResults = associativeProcessor.arithmeticsLogic();
+        for (Map.Entry<Integer, int[]> entry : arithmeticResults.entrySet()) {
+            System.out.println(entry.getKey() + ": " + Arrays.toString(entry.getValue()));
         }
     }
 }
